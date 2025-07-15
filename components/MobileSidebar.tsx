74e841c52cdc66956f2380268278b6fa4e8250a2
Tega96 <otiteotega@gmail.com>
2025-07-15 22:48:30 +0100

    add nav item component, implement toggle click

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,15 +1,38 @@
+//ts-none
+import { SidebarComponent } from '@syncfusion/ej2-react-navigations';
+import { Link } from 'react-router'
+import NavItems from './NavItems';
 
 function MobileSidebar() {
-    return (
-        <section className="nav-items">
-            <div className="link-logo">
-                <div>
-                    <img src="/assets/icons/logo.svg" alt="logo" />
-                </div>
-                <h2>Tourvisto</h2>
+    let sidebar: SidebarComponent
 
-            </div>
-        </section>
+    function toggleClick() {
+        sidebar.toggle();
+    }
+
+    return (
+        <div className="mobile-sidebar wrapper">
+            <header className="">
+                <Link to="/dashboard">
+                    <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+                    <h2>Tourvisto</h2>
+                </Link>
+                <button onClick={toggleClick}>
+                    <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
+                </button>
+            </header>
+
+            <SidebarComponent 
+                width={270}
+                ref={(Sidebar) => sidebar = Sidebar}
+                created={() => sidebar.hide()}
+                closeOnDocumentClick={true}
+                showBackdrop={true}
+                type="over"
+            >
+                <NavItems />
+            </SidebarComponent>
+        </div>
     );
 }
 export default MobileSidebar;
